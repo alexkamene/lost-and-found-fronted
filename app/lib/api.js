e417c23a0aecdeb5af283a182863e8adb23b7b63
alexkamene <kamenealexmumo@gmail.com>
2025-09-01 14:23:23 +0300

    added backed server

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://lost-and-found-backend-0z7d.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
